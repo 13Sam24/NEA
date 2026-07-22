b0455ae3ecee71d101a112855cc58425d3ff3382
13Sam24 <richesam12@gmail.com>
2026-07-22 20:16:57 +0100

    home page now works

--- Screens/home.py
+++ Screens/home.py
@@ -8,4 +8,20 @@ class HomeScreen:
         self.startImage = startImage
         self.optionImage = optionImage
         self.quitImage = quitImage
-        startButton = Button(self.screen, 710, 540, 500, 150, self.startImage)
+
+    def show(self):
+        self.startButton = Button(self.screen, 710, 440, 500, 150, self.startImage)
+        self.optionButton = Button(self.screen, 710, 635, 500, 150, self.optionImage)
+        self.quitButton = Button(self.screen, 710, 830, 500, 150, self.quitImage)
+
+    def buttonPress(self):
+        if self.startButton.clicked(pygame.mouse.get_pos(), pygame.mouse.get_pressed()) == True:
+            return 'LevelsScreen'
+
+        if self.optionButton.clicked(pygame.mouse.get_pos(), pygame.mouse.get_pressed()) == True:
+                    return 'Options'
+
+        if self.quitButton.clicked(pygame.mouse.get_pos(), pygame.mouse.get_pressed()) == True:
+                    return 'Quit'        
+
+    
--- main.py
+++ main.py
@@ -9,16 +9,33 @@ pygame.init()
 
 screen = pygame.display.set_mode((1920, 1080))
 pygame.display.set_caption('Platformer')
-WHITE = (255, 0, 0)
+WHITE = (255, 255, 255)
 screen.fill(WHITE)
 
 #Initialising the screens as objects
-homeScreen = HomeScreen(screen, 'Assets\HomeScreen\Start.png', 'OptionsButton.png', 'Quit.png')
+homeScreen = HomeScreen(screen, 'Assets\HomeScreen\Start.png', 'Assets\HomeScreen\OptionsButton.png', 'Assets\HomeScreen\Quit.png')
+
+currentScreen = 'HomeScreen'
 
 running = True
 while running:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
+
+	if currentScreen == 'HomeScreen':
+		homeScreen.show()
+		pageSelect = homeScreen.buttonPress()
+
+		if pageSelect == 'LevelsScreen':
+			currentScreen = 'LevelsScreen'
+		if pageSelect == 'Options':
+			currentScreen = 'Options'
+		if pageSelect == 'Quit':
+			currentScreen = 'Quit'
+			running = False
+		
+
+	pygame.display.flip()
 
 pygame.quit()
